4a2f11be861e025cd17e0076500634d453e10daf
Agustín Gañán Recio <agusgr@gmail.com>
2015-05-08 17:07:40 +0200

    tabs

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,11 +3,30 @@
 	var fuzzyApp = angular.module('fuzzyApp', []);
 	fuzzyApp.controller('FuzzyController', function() {
 		this.users = users;
+
+		this.toggleAdmin = function(me) {
+			users[me + 1].admin = false
+		};
+
 	});
 
+
+  	fuzzyApp.controller('TabController', function(){
+    	this.tab = 1;
+
+    	this.setTab = function(newValue){
+    	  this.tab = newValue;
+    	};
+
+    	this.isSet = function(tabName){
+      		return this.tab === tabName;
+    	};
+  	});
+
+
 	  var users = [
-	    { name: 'Bruce', psw: 295, surname : 'Wayne', admin : true },
-	    { name: 'Peter', psw: 595, surname : 'Parker', admin : true },
-	    { name: 'Clark', psw: 395, surname : 'Kent', admin : true}
+	    { hero : 'Batman', name: 'Bruce', psw: 295, surname : 'Wayne', admin : true },
+	    { hero : 'Spiderman', name: 'Peter', psw: 595, surname : 'Parker', admin : true },
+	    { hero : 'Superman', name: 'Clark', psw: 395, surname : 'Kent', admin : true}
 	  ];
 })();
